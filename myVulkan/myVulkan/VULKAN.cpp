cc9565e326259ce4948466c08e60f9437dfe5335
neohazardous <ol29@njit.edu>
2018-10-08 17:01:46 -0400

    Minor updates to my version of the vulkan project

--- myVulkan/myVulkan/VULKAN.cpp
+++ myVulkan/myVulkan/VULKAN.cpp
@@ -6,12 +6,12 @@
 #include <functional> //will be used for a lambda functions in the resource mangement sections 
 #include <cstdlib> //This header provides exit_success/exit_failure macros
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
 class TriangleRend {
 	
 	//make constants for window creation vs just hard coding the window size
-	const int WIDTH = 800; 
-	const int HEIGHT = 600;
-	GLFWwindow * window;
 
 public:
 	void run() {
@@ -31,6 +31,8 @@ private:
 	//vulkan objects will be stored as private class members. functions will be used to initiate them
 	//which will be called from the initVulkan function.
 private:
+	GLFWwindow * window;
+	VkInstance instance;
 
 	void initWindow() {
 		
@@ -42,6 +44,8 @@ private:
 
 	void initVulkan() {
 
+		createInstance();
+
 	}
 
 	//mainLoop will interate rendering frames until the window is closed
@@ -52,7 +56,6 @@ private:
 			glfwPollEvents();
 
 		}
-
 	}
 
 	//cleanup is used to deallocate resources
@@ -60,6 +63,37 @@ private:
 
 		glfwDestroyWindow(window);
 		glfwTerminate();
+
+	}
+
+	void createInstance() {
+
+		VkApplicationInfo appInfo = {};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello world";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtCount = 0;
+		const char ** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtCount);
+
+		createInfo.enabledExtensionCount = glfwExtCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+		createInfo.enabledLayerCount = 0;
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+
+			throw std::runtime_error("failed to create instance");
+
+		}
 
 	}
 };
